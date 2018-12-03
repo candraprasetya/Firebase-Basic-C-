36dc09b11194ad379f254fff9e8edcc6daeada8f
CANDRA RAMADHAN <candraramadhanp@gmail.com>
2018-12-04 05:07:29 +0700

    Add project files.

---
+++ FirebaseLearn/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+
+namespace FirebaseLearn
+{
+    public partial class Form1 : Form
+    {
+
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "c9oua6rAONSy6QygoBcEKzJJwrc3RQbLpgwELZDt",
+            BasePath = "https://fir-loginregister-a9758.firebaseio.com/"
+        };
+
+        IFirebaseClient client;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new FireSharp.FirebaseClient(config);
+
+            if(client != null)
+            {
+                MessageBox.Show("Connection is established");
+            }
+        }
+    }
+}
